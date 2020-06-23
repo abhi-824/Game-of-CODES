c9682199e28e387d41dccc1c7272415e426d2fdc
MuskanGupta1503 <66381138+MuskanGupta1503@users.noreply.github.com>
2020-06-23 12:06:52 +0530

    Update script.js
    jquery code for giving unsolved questions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
 // Write js here
+$(document).ready(function(){
+  
+    const url="https://codeforces.com/api/user.status?handle="
+    
+      async function getdata(){
+          $("ul").empty()
+        let modified_url=url+$("#test").val()
+          const jsondata=await fetch(modified_url)
+          const jsdata=await jsondata.json()
+          console.log(jsdata.result)
+          let unsolved=new Set()
+          let solved=new Set()
+          unsolved.clear()
+          solved.clear()
+    
+          for(let i=0;i<jsdata.result.length;i++)
+          {
+              if(jsdata.result[i].verdict=="OK")
+              {
+                  let str=jsdata.result[i].problem.contestId+"-"+jsdata.result[i].problem.index
+                  solved.add(
+                      str
+                  )
+    
+                  if(unsolved.has(str))
+                  {
+                      unsolved.delete(str)
+                  }
+    
+              }
+              else
+              {
+                let str=jsdata.result[i].problem.contestId+"-"+jsdata.result[i].problem.index
+                if(!solved.has(str))
+                {unsolved.add(
+                    // jsdata.result[i].problem.contestId,
+                    // contestIndex:jsdata.result[i].problem.index
+                    str
+                  )
+                }
+              }
+          }
+          console.log(solved)
+          console.log(unsolved)
+          for(let i of unsolved)
+          {
+              console.log(i)
+          }
+          for(let i of unsolved)
+      {
+        $("ul").append(i+"<br>")
+      }
+      }
+      getdata()
+    // $("button").click(getdata())
+    $("button").click(function(){getdata()})
+    
+    });
+    
