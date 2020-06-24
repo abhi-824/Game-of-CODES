e4f3c0d5150b02dd32e0b02bab587c9c61851032
abhi-824 <55046095+abhi-824@users.noreply.github.com>
2020-06-24 14:31:38 +0530

    Update calculations.js

--- js/calculations.js
+++ js/calculations.js
@@ -1,11 +1,19 @@
 let handle='';
-let url='dashboard.html?handle=';
+let url='file:///F:/Game-of-CODES-master/dashboard.html?handle=';
 let button=document.querySelector('#handle_button');
 button.addEventListener('click',function(e){
     handle=document.querySelector('#handle').value;
     url=url+handle;
     console.log(handle);
-    document.location.href = url;
+    if(!handle)
+    {
+        alert("Please enter User handle");
+    }
+    else{
+        document.location.href = url;
+
+    }
+
     e.preventDefault();
 })
 
