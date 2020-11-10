bed9bc1e5831547617cb61af94b3b9cfefde775e
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2020-11-10 11:29:38 +0530

    added visualisers scripts and some styling

--- publicis/js/animations/vis.js
+++ publicis/js/animations/vis.js
@@ -2,21 +2,21 @@ function update_cell(x, y) {
   // console.log(x);
   // console.log(y);
   window.setTimeout(function () {
-    let no = (x) * box_no + y;
-    
-    cell[no-1].style.background = "purple";
+    let no = x * box_no + y;
+    cell[no - 1].style.background = "purple";
   }, (c_delay += delay_time));
 }
 function update_final(newX, newY) {
   window.setTimeout(function () {
     cell[(newX - 1) * box_no + newY - 1].style.background = "yellow";
   }, (c_delay += delay_time));
-  console.log("h");
 }
 function reconstruct(i) {
   window.setTimeout(function () {
     if (i != 0) {
       let no = i[0] * box_no + i[1];
+      console.log(typeof i);
+      console.log(no);
       cell[no - 1].style.background = "yellow";
     }
   }, (c_delay += delay_time));
