221c51fb888da39496421d57e572a7450c1a5d0e
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2021-01-10 16:40:21 +0530

    added animations

--- publicis/annnual_report/app.js
+++ publicis/annnual_report/app.js
@@ -1,4 +1,11 @@
 document.querySelector('#handle_submit').addEventListener("submit",(e)=>{
     e.preventDefault();
-    
+    document.querySelector('.go_up').classList.add('animate__backOutUp')
+    document.querySelector('.go_down').classList.add('animate__backOutDown')    
+    document.querySelector('.go_down').addEventListener('animationend',()=>{
+        
+        document.querySelector('.header').style.height ='0vh';
+    })
+    let handle=document.querySelector('#handle').value;
+
 })
